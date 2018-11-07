bc24b164869835bc14550cf6c85f381c778e03a3
Gaëtan Masson <im.gaetanmasson@gmail.com>
2018-11-07 18:00:58 +0100

    Add system tests about CRUD operations on Fighter

--- test/system/fighters_test.rb
+++ test/system/fighters_test.rb
@@ -1,39 +1,64 @@
 require "application_system_test_case"
 
 class FightersTest < ApplicationSystemTestCase
-  setup do
-    @fighter = fighters(:frodo)
-  end
-
   test "visiting the index" do
     visit fighters_url
-    assert_selector "h1", text: "Fighters"
+    assert_text "Frodo"
+    assert_text "Gandalf"
+  end
+
+  test "showing a Fighter" do
+    visit fighters_url
+    click_on "Show", match: :first
+
+    assert_text "Name: Frodo"
   end
 
   test "creating a Fighter" do
     visit fighters_url
     click_on "New Fighter"
 
-    fill_in "Attack", with: @fighter.attack
-    fill_in "Health", with: @fighter.health
-    fill_in "Name", with: @fighter.name
+    fill_in "Attack", with: 50
+    fill_in "Health", with: 50
+    fill_in "Name", with: "Frodo"
     click_on "Create Fighter"
 
     assert_text "Fighter was successfully created"
-    click_on "Back"
+  end
+
+  test "creating a Fighter with bad data" do
+    visit fighters_url
+    click_on "New Fighter"
+
+    fill_in "Attack", with: 0
+    fill_in "Health", with: 0
+    fill_in "Name", with: ""
+    click_on "Create Fighter"
+
+    assert_text "prohibited this fighter from being saved"
   end
 
   test "updating a Fighter" do
     visit fighters_url
     click_on "Edit", match: :first
 
-    fill_in "Attack", with: @fighter.attack
-    fill_in "Health", with: @fighter.health
-    fill_in "Name", with: @fighter.name
+    fill_in "Name", with: "Sauron"
     click_on "Update Fighter"
 
     assert_text "Fighter was successfully updated"
     click_on "Back"
+  end
+
+  test "updating a Fighter with bad data" do
+    visit fighters_url
+    click_on "Edit", match: :first
+
+    fill_in "Attack", with: 0
+    fill_in "Health", with: 0
+    fill_in "Name", with: ""
+    click_on "Update Fighter"
+
+    assert_text "prohibited this fighter from being saved"
   end
 
   test "destroying a Fighter" do
